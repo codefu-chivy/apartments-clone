80709ceb29a1d25da6f14a9e4900813c2170915c
Chival Trotman <chivy360@gmail.com>
2017-06-21 03:31:03 -0400

    add input collapse logic

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,4 +1,8 @@
 $(function() {
+    var collapse = true;
+    var collapseBedBath = true;
+    var minAmount, maxAmount, maxVal, minVal;
+
     $.get("/is-secure", function(data) {
         var url;
         if (data.secure) {
@@ -51,5 +55,163 @@ $(function() {
         })
     });
     }
-    
+
+    //Input dropdowns on click
+    $("#rent-range").click(function() {
+        if (collapse) {
+            $(".input-collapse").addClass("input-collapse-drop");
+            $(".section-collapse").addClass("min-column-drop");
+            $(".arrow-rent").addClass("fa-arrow-down-rotate");
+        }
+        else {
+            $(".input-collapse").removeClass("input-collapse-drop");
+            $(".section-collapse").removeClass("min-column-drop");
+            $(".arrow-rent").removeClass("fa-arrow-down-rotate");
+        }
+        collapse = !collapse;  
+        
+    });
+    $(".min-option").click(function() {
+        showMax(event, $(this));
+    });
+    $(".max-option").click(function() {
+        showMin(event, $(this));
+    });
+    $("#max").on({"click": showMax, "keypress": function(e) {
+        if (e.key === "Enter") {
+            if (!e.target.value) {
+                return;
+            }
+            else {
+                showMin(event, $(this));
+            }
+        }
+        if (e.key === "0" && !$("#max").val()) {
+            e.preventDefault();
+        }
+        if ((!Number(e.key) && e.key !== "0") || e.target.value.length >= 5) {
+            e.preventDefault();
+        }
+        
+    }});
+    $("#min").on({"click": showMin, "keypress": function(e) {
+        if (e.key === "Enter") {
+            if (!e.target.value) {
+                return;
+            }
+            else {
+                showMax(event, $(this));
+            }
+        }
+        if (e.key === "0" && !$("#max").val()) {
+            e.preventDefault();
+        }
+        if ((!Number(e.key) && e.key !== "0") || e.target.value.length >= 5) {
+            e.preventDefault();
+        }
+    }});
+    $("#bed-bath").click(function() {
+        if (collapseBedBath) {
+            $(".section-collapse-bb").addClass("input-collapse-drop");
+            $(".arrow-bed-bath").addClass("fa-arrow-down-rotate");
+        }
+        else {
+            $(".section-collapse-bb").removeClass("input-collapse-drop");
+            $(".arrow-bed-bath").removeClass("fa-arrow-down-rotate");
+        }
+        collapseBedBath = !collapseBedBath;
+    });
+    $(".section-collapse-bb option").click(function() {
+        handleBedBath($(this));
+    });
+
+    //Functions to handle min and max rent and bed and bath
+    function showMax(event, obj) {
+        if (obj) {
+            if (Number(obj.val().slice(1, obj.val().length))) {
+                minAmount = obj.val();
+                if ($("#max").val()) {
+                    var maxText = $("#max").val();
+                }
+                if (minAmount.indexOf("$") === -1) {
+                    minAmount = "$" +  obj.val();
+                }
+                if (maxText) {
+                    $("#rent-range").html(minAmount + "-$" + maxText);
+                }
+                else {
+                    $("#rent-range").html(minAmount + "+");
+                }
+            }
+            else {
+                if (maxVal) {
+                    $("#rent-range").html("<" + maxVal)
+                }
+                else {
+                    $("#rent-range").html("Rent Range");
+                }
+            }
+        }
+        $("#max-column").css("display", "initial");
+        $("#min-column").css("display", "none");
+    }
+
+    function showMin(event, obj) {
+        var text;
+        if (obj) {
+            if (Number(obj.val())) {
+                maxVal = obj.val();
+            }
+            else {
+                maxVal = Number(obj.val().slice(1, obj.val().length));
+            }
+            if (maxVal) {
+                maxAmount = obj.val();
+                if (!minAmount && !$("#min").val()) {
+                    $("#rent-range").html("<$" + maxVal);
+                }
+                else {
+                    if (!minAmount) {
+                        minVal = Number($("#min").val());
+                        $("#rent-range").html("$" + minVal + "-$" + maxVal);
+                    }
+                    else {
+                        minVal = Number(minAmount.slice(1, minAmount.length));
+                        text = $("#rent-range").text();
+                        text = text.slice(0, text.length - 1);
+                        $("#rent-range").html(text + "-$" + maxVal);
+                    }
+                    if (minVal >= maxVal) {
+                        alert("Minimum can't be greater or equal to maximum value!");
+                        $("#rent-range").html("Rent Range");
+                        return;
+                    }
+                }
+            }
+            $(".input-collapse").removeClass("input-collapse-drop");
+            collapse = !collapse;
+            minAmount = null;
+        }
+        else {
+            $("#rent-range").html("Rent Range");
+            $("#min-column").css("display", "initial");
+            $("#max-column").css("display", "none");
+        }
+    }
+
+    function handleBedBath(ele) {
+        var selectedBed;
+        var selectedBath;
+
+        if (ele.parent().attr("id") === "beds") {
+            selectedBed = ele.val();
+            selectedBath = document.getElementById("baths").value;
+            $("#bed-bath").html(selectedBed + " x " + selectedBath);
+        }
+        else {
+            selectedBath = ele.val();
+            selectedBed = document.getElementById("beds").value;
+            $("#bed-bath").html(selectedBed + " x " + selectedBath);
+        }
+    }
 });
